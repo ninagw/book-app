2b7539812fd3844d9247d05e877360ff17f5836b
ninagw <weber_nina@web.de>
2025-01-06 18:05:30 +0100

    add TBR Button to Details page

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -11,6 +11,14 @@ const StyledBookmarkButton = styled.button`
   padding: 0.4rem;
 `;
 
+const StyledTBRButton = styled.button `
+  background-color: ${({ $isTBR }) =>
+    $isTBR ? "var(--pink-color)" : "var(--lightgrey-color)"};
+  border-radius: var(--border-radius);
+  border: none;
+  padding: 0.4rem;
+`;
+
 const StyledAlreadyReadButton = styled.button`
   background-color: ${({ $isAlreadyRead }) =>
     $isAlreadyRead ? "var(--pink-color)" : "var(--lightgrey-color)"};
@@ -27,7 +35,7 @@ const StyledCurrentlyReadingButton = styled.button`
   padding: 0.4rem;
 `;
 
-export default function BookDetailsPage({books, booksData, handleToggleBookmark, handleToggleAlreadyRead, handleToggleCurrentlyReading}) {
+export default function BookDetailsPage({books, booksData, handleToggleBookmark, handleToggleTBR, handleToggleAlreadyRead, handleToggleCurrentlyReading}) {
 const router = useRouter();
 const { id } = router.query;
 const pathName = router.pathname;
@@ -71,6 +79,14 @@ console.log("ROUTER QUERY ID: ", id); // aktuell nur first book id, da hart geco
             >
               Set book on wishlist
         </StyledBookmarkButton>
+        <StyledTBRButton 
+            type="button"
+            onClick={() => handleToggleTBR(id)}
+            $isTBR={currentBookData?.isTBR}
+            aria-label={currentBookData?.isTBR ? "remove from your to be read list" : "add to your to be read list"}
+            >
+              Set on your TBR list
+        </StyledTBRButton> 
         <StyledAlreadyReadButton 
             type="button"
             onClick={() => handleToggleAlreadyRead(id)}
